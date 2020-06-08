192669aa4d76c7463560acdf35f614ba213fea85
ABeltea <hild_94@hotmail.com>
2020-06-08 14:12:43 +0200

    First step

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
-import { Component, VERSION } from '@angular/core';
+import {Component} from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
+import { MatIconRegistry } from '@angular/material/icon';
+import {MatGridListModule} from '@angular/material/grid-list'; 
 
 @Component({
   selector: 'my-app',
@@ -6,5 +9,43 @@ import { Component, VERSION } from '@angular/core';
   styleUrls: [ './app.component.css' ]
 })
 export class AppComponent  {
-  name = 'Angular ' + VERSION.major;
+  name = 'Starter Template';
+
+  routes: [{
+      icon: 'home',
+      route: '.',
+      title: 'Home',
+    }, {
+      icon: 'library_books',
+      route: '.',
+      title: 'Documentation',
+    }, {
+      icon: 'color_lens',
+      route: '.',
+      title: 'Style Guide',
+    }, {
+      icon: 'view_quilt',
+      route: '.',
+      title: 'Layouts',
+    }, {
+      icon: 'picture_in_picture',
+      route: '.',
+      title: 'Components & Addons',
+    },
+  ];
+  usermenu: [{
+      icon: 'swap_horiz',
+      route: '.',
+      title: 'Switch account',
+    }, {
+      icon: 'tune',
+      route: '.',
+      title: 'Account settings',
+    }, {
+      icon: 'exit_to_app',
+      route: '.',
+      title: 'Sign out',
+    },
+  ];
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,35 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
+
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatCardModule } from '@angular/material/card';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import {MatGridListModule} from '@angular/material/grid-list'; 
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  imports:      [
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    RouterModule.forRoot([]),
+    HttpClientModule,
+    /** Material Modules */
+    MatButtonModule,
+    MatIconModule,
+    MatListModule,
+    MatCardModule,
+    MatToolbarModule,
+    MatGridListModule
+    ],
+  declarations: [ AppComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
